fixed non-zero bloackage on rotor plane and layout term calculation

py_wake/deficit_models/vortexcylinder.py
@@ -52,7 +52,7 @@ class VortexCylinder(BlockageDeficitModel):
         # term 1
         # non-zero for rho < R
         term1_ijlk = np.zeros_like(rho_ijlk)
-        term1_ijlk[rho_ijlk < 1.] = 1.
+        term1_ijlk[(rho_ijlk < 1.) & (abs(xi_ijlk) > self.limiter)] = 1.
         # term 2
         # zero for xi==0, thus avoid computation
         ic = (abs(xi_ijlk) > self.limiter)
@@ -75,9 +75,10 @@ class VortexCylinder(BlockageDeficitModel):
         """
         The analytical relationships can be found in [1,2], in particular equations (7-8) from [1].
         """
-        if not self.deficit_initalized:
-            # calculate layout term, self.dmu_G_ijlk
-            self._calc_layout_terms(D_src_il, dw_ijlk, cw_ijlk)
+
+        # if not self.deficit_initalized:
+        #    # calculate layout term, self.dmu_G_ijlk
+        self._calc_layout_terms(D_src_il, dw_ijlk, cw_ijlk)
 
         # circulation/strength of vortex cylinder
         gammat_ilk = WS_ilk * 2. * a0(ct_ilk)

py_wake/ground_models/ground_models.py
@@ -26,10 +26,14 @@ class Mirror(GroundModel):
         if 'cw_ijlk' in kwargs:
             cw_ijlk_mirror = np.sqrt(dh_ijlk_mirror**2 + kwargs['hcw_ijlk']**2)
         above_ground = ((kwargs['h_il'][:, na, :, na] + kwargs['dh_ijlk']) > 0)
-        return np.array([calc_deficit(**kwargs),
-                         calc_deficit(dh_ijlk=dh_ijlk_mirror,
+        mirror_deficit = calc_deficit(dh_ijlk=dh_ijlk_mirror,
                                       cw_ijlk=cw_ijlk_mirror,
-                                      **{k: v for k, v in kwargs.items() if k not in ['dh_ijlk', 'cw_ijlk']})]) * above_ground[na]
+                                      **{k: v for k, v in kwargs.items() if k not in ['dh_ijlk', 'cw_ijlk']})
+        # ensure the dificit on itself to be zero
+        idiag = np.diag_indices(mirror_deficit.shape[0])
+        mirror_deficit[idiag] = 0.
+        return np.array([calc_deficit(**kwargs),
+                         mirror_deficit]) * above_ground[na]
 
     def __call__(self, calc_deficit, **kwargs):
         return np.sum(self._calc(calc_deficit, **kwargs), 0)